97847b7baff3c50e81fa212f1ff6e7b27dc7f42b
neuman <owiroadams@gmail.com>
2024-04-23 16:43:40 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-
-from flask import Flask, make_response, jsonify, session
+from flask import Flask, make_response, jsonify, session, abort
 from flask_migrate import Migrate
 
 from models import db, Article, User
@@ -22,13 +20,26 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+    articles = Article.query.all()
+    return jsonify([article.serialize for article in articles])
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    # Check if session['page_views'] exists, if not, set it to 0
+    session['page_views'] = session.get('page_views', 0)
 
-    pass
+    # Increment the page views for this session
+    session['page_views'] += 1
+
+    # Check if page views exceed the limit
+    if session['page_views'] > 3:
+        return jsonify({'message': 'Maximum pageview limit reached'}), 401
+
+    # Retrieve the article by ID
+    article = Article.query.get_or_404(id)
+    
+    return jsonify(article.serialize)
 
 if __name__ == '__main__':
     app.run(port=5555)
+
